8a5f2091ffe864babbb700e0c5620f22067a660c
chocoapricot <chocoapricot@nifty.com>
2025-07-20 03:32:02 +0900

    home, csstest, apiアプリの全ビューをログイン必須化

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,10 +1,29 @@
+def get_company_info(request):
+    corporate_number = request.GET.get("corporate_number")
+    if corporate_number:
+        company_info = fetch_company_info(corporate_number)
+        if company_info:
+            return JsonResponse({"success": True, "data": company_info})
+    return JsonResponse({"success": False, "error": "企業情報の取得に失敗しました。"})
+def get_zipcode_info(request):
+    zipcode = request.GET.get("zipcode")
+    # logger.error(zipcode)
+    if zipcode:
+        zipcode_info = fetch_zipcode(zipcode)
+        # logger.error(zipcode_info)
+        if zipcode_info:
+            return JsonResponse({"success": True, "data": zipcode_info})
+    return JsonResponse({"success": False, "error": "郵便番号の取得に失敗しました。"})
+
 import logging
 from django.http import JsonResponse
 from apps.api.helpers import fetch_company_info, fetch_zipcode  # API呼び出し関数をインポート
+from django.contrib.auth.decorators import login_required
 
 # ロガーの作成
 logger = logging.getLogger('api')
 
+@login_required
 def get_company_info(request):
     corporate_number = request.GET.get("corporate_number")
     if corporate_number:
@@ -13,6 +32,7 @@ def get_company_info(request):
             return JsonResponse({"success": True, "data": company_info})
     return JsonResponse({"success": False, "error": "企業情報の取得に失敗しました。"})
 
+@login_required
 def get_zipcode_info(request):
     zipcode = request.GET.get("zipcode")
     # logger.error(zipcode)
--- apps/csstest/views.py
+++ apps/csstest/views.py
@@ -1,7 +1,10 @@
+
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def list(request):
     return render(request, 'staff/simplex_list.html')
 
