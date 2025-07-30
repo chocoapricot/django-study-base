85afd116395ce0aac0cb130cbbec64742ebf5a40
chocoapricot <chocoapricot@nifty.com>
2025-07-30 23:09:25 +0900

    Changes to be committed: 	modified:   apps/accounts/adapters.py 	modified:   templates/home/home.html

--- apps/accounts/adapters.py
+++ apps/accounts/adapters.py
@@ -1,4 +1,15 @@
 from allauth.account.adapter import DefaultAccountAdapter
 
 class CustomAccountAdapter(DefaultAccountAdapter):
-    pass
+    def save_user(self, request, user, form, commit=True):
+        # まずallauthのデフォルトの保存処理を実行
+        user = super().save_user(request, user, form, commit=False)
+
+        # その後、カスタムフィールドとユーザー名を更新
+        user.last_name = form.cleaned_data.get('last_name')
+        user.first_name = form.cleaned_data.get('first_name')
+        user.username = user.email  # メールアドレスをユーザー名に設定
+
+        if commit:
+            user.save()
+        return user
