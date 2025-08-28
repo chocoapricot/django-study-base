9fbd93d372d60f313833151a6306f2c0a6f1f6b0
chocoapricot <chocoapricot@nifty.com>
2025-08-29 01:50:49 +0900

    Changes to be committed: 	modified:   apps/staff/signals.py

--- apps/staff/signals.py
+++ apps/staff/signals.py
@@ -1,2 +1,50 @@
-# This file is intentionally left blank.
-# The generic signal handlers in apps/common/signals.py are used for logging.
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django_currentuser.middleware import get_current_user
+from apps.system.logs.utils import log_model_action
+from .models import StaffQualification, StaffSkill, StaffFile
+
+
+@receiver(post_save, sender=StaffQualification)
+def log_staff_qualification_save(sender, instance, created, **kwargs):
+    """スタッフ資格の作成・更新ログ"""
+    user = get_current_user()
+    action = 'create' if created else 'update'
+    log_model_action(user, action, instance, getattr(instance, 'version', None))
+
+
+@receiver(post_delete, sender=StaffQualification)
+def log_staff_qualification_delete(sender, instance, **kwargs):
+    """スタッフ資格の削除ログ"""
+    user = get_current_user()
+    log_model_action(user, 'delete', instance)
+
+
+@receiver(post_save, sender=StaffSkill)
+def log_staff_skill_save(sender, instance, created, **kwargs):
+    """スタッフ技能の作成・更新ログ"""
+    user = get_current_user()
+    action = 'create' if created else 'update'
+    log_model_action(user, action, instance, getattr(instance, 'version', None))
+
+
+@receiver(post_delete, sender=StaffSkill)
+def log_staff_skill_delete(sender, instance, **kwargs):
+    """スタッフ技能の削除ログ"""
+    user = get_current_user()
+    log_model_action(user, 'delete', instance)
+
+
+@receiver(post_save, sender=StaffFile)
+def log_staff_file_save(sender, instance, created, **kwargs):
+    """スタッフファイルの作成・更新ログ"""
+    user = get_current_user()
+    action = 'create' if created else 'update'
+    log_model_action(user, action, instance, getattr(instance, 'version', None))
+
+
+@receiver(post_delete, sender=StaffFile)
+def log_staff_file_delete(sender, instance, **kwargs):
+    """スタッフファイルの削除ログ"""
+    user = get_current_user()
+    log_model_action(user, 'delete', instance)
