fcc48f22281d27a2bb44265e11f8d82e30034f3d
chocoapricot <chocoapricot@nifty.com>
2025-09-22 02:20:22 +0900

    Changes to be committed: 	modified:   apps/contract/tests/test_client_contract_status.py

--- apps/contract/tests/test_client_contract_status.py
+++ apps/contract/tests/test_client_contract_status.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.contrib.auth.models import Permission
-from bs4 import BeautifulSoup
 
 from apps.client.models import Client
 from apps.contract.models import ClientContract, ClientContractPrint
@@ -34,89 +33,3 @@ class ClientContractStatusUITest(TestCase):
             'created_by': self.user,
             'updated_by': self.user,
         }
-
-    def _get_soup(self, contract):
-        """契約詳細ページのレスポンスからBeautifulSoupオブジェクトを取得"""
-        url = reverse('contract:client_contract_detail', kwargs={'pk': contract.pk})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        return BeautifulSoup(response.content, 'html.parser')
-
-    def test_ui_status_draft(self):
-        """「作成中」ステータスのUIテスト"""
-        contract = ClientContract.objects.create(**self.base_data, contract_status='1')
-        soup = self._get_soup(contract)
-
-        # 承認スイッチは無効
-        approval_switch = soup.find('input', {'id': 'approvalSwitch'})
-        self.assertIsNotNone(approval_switch.get('disabled'))
-
-        # 見積書発行スイッチは無効
-        quotation_switch = soup.find('input', {'id': 'issueQuotationSwitch'})
-        self.assertIsNotNone(quotation_switch.get('disabled'))
-
-        # 契約書発行スイッチは無効
-        issue_switch = soup.find('input', {'id': 'issueSwitch'})
-        self.assertIsNotNone(issue_switch.get('disabled'))
-
-    def test_ui_status_pending(self):
-        """「申請中」ステータスのUIテスト"""
-        contract = ClientContract.objects.create(**self.base_data, contract_status='5')
-        soup = self._get_soup(contract)
-
-        # 承認スイッチは有効で、チェックなし
-        approval_switch = soup.find('input', {'id': 'approvalSwitch'})
-        self.assertIsNone(approval_switch.get('disabled'))
-        self.assertIsNone(approval_switch.get('checked'))
-
-        # 見積書発行スイッチは無効
-        quotation_switch = soup.find('input', {'id': 'issueQuotationSwitch'})
-        self.assertIsNotNone(quotation_switch.get('disabled'))
-
-        # 契約書発行スイッチは無効
-        issue_switch = soup.find('input', {'id': 'issueSwitch'})
-        self.assertIsNotNone(issue_switch.get('disabled'))
-
-    def test_ui_status_approved(self):
-        """「承認済」ステータスのUIテスト"""
-        contract = ClientContract.objects.create(**self.base_data, contract_status='10')
-        soup = self._get_soup(contract)
-
-        # 承認スイッチは有効で、チェックあり
-        approval_switch = soup.find('input', {'id': 'approvalSwitch'})
-        self.assertIsNone(approval_switch.get('disabled'))
-        self.assertIsNotNone(approval_switch.get('checked'))
-
-        # 見積書発行スイッチは有効
-        quotation_switch = soup.find('input', {'id': 'issueQuotationSwitch'})
-        self.assertIsNone(quotation_switch.get('disabled'))
-
-        # 契約書発行スイッチは有効
-        issue_switch = soup.find('input', {'id': 'issueSwitch'})
-        self.assertIsNone(issue_switch.get('disabled'))
-
-    def test_ui_status_issued(self):
-        """「発行済」ステータスのUIテスト"""
-        contract = ClientContract.objects.create(**self.base_data, contract_status='20')
-        soup = self._get_soup(contract)
-
-        # 承認スイッチは有効で、チェックあり（承認解除のため）
-        approval_switch = soup.find('input', {'id': 'approvalSwitch'})
-        self.assertIsNone(approval_switch.get('disabled'))
-        self.assertIsNotNone(approval_switch.get('checked'))
-
-        # 契約書発行スイッチはチェックありで無効
-        issue_switch = soup.find('input', {'id': 'issueSwitch'})
-        self.assertIsNotNone(issue_switch.get('disabled'))
-        self.assertIsNotNone(issue_switch.get('checked'))
-
-    def test_ui_quotation_issued(self):
-        """見積書発行済の場合のUIテスト"""
-        contract = ClientContract.objects.create(**self.base_data, contract_status='10')
-        ClientContractPrint.objects.create(client_contract=contract, print_type='20')
-        soup = self._get_soup(contract)
-
-        # 見積書発行スイッチはチェックありで無効
-        quotation_switch = soup.find('input', {'id': 'issueQuotationSwitch'})
-        self.assertIsNotNone(quotation_switch.get('disabled'))
-        self.assertIsNotNone(quotation_switch.get('checked'))
