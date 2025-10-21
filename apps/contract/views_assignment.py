ec88b306349e7648f006adaf0320cd0481e718a2
chocoapricot <chocoapricot@nifty.com>
2025-10-22 03:54:08 +0900

    Changes to be committed: 	modified:   apps/contract/views_assignment.py 	new file:   templates/contract/_contract_timeline_chart.html 	modified:   templates/contract/client_assignment_confirm.html 	modified:   templates/contract/client_staff_assignment_detail_list.html 	modified:   templates/contract/staff_assignment_confirm.html

--- apps/contract/views_assignment.py
+++ apps/contract/views_assignment.py
@@ -250,12 +250,22 @@ def staff_assignment_confirm(request):
         # 粗利率計算
         profit_margin_info = _calculate_profit_margin(client_contract, staff_contract)
 
+        # 既存の割当済みスタッフ契約を取得（スタッフ名＞開始日でソート）
+        existing_assignments = ContractAssignment.objects.filter(
+            client_contract=client_contract
+        ).select_related('staff_contract__staff', 'staff_contract__employment_type').order_by(
+            'staff_contract__staff__name_last', 
+            'staff_contract__staff__name_first', 
+            'staff_contract__start_date'
+        )
+
         context = {
             'client_contract': client_contract,
             'staff_contract': staff_contract,
             'from_view': 'client',
             'show_daily_dispatch_warning': show_daily_dispatch_warning,
             'profit_margin_info': profit_margin_info,
+            'existing_assignments': existing_assignments,
         }
 
         return render(request, 'contract/staff_assignment_confirm.html', context)
