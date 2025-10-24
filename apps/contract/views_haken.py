f0a4241904dbfd1ea1330ca0d7248c3d3522f0fa
chocoapricot <chocoapricot@nifty.com>
2025-10-25 00:18:41 +0900

    Changes to be committed: 	modified:   apps/contract/views_haken.py

--- apps/contract/views_haken.py
+++ apps/contract/views_haken.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.core.paginator import Paginator
 from django.core.files.base import ContentFile
 from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
