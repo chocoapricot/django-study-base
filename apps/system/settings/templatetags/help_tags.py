c72b4874fd1d1c2b3fe6a0e04157a9ba3f76f701
chocoapricot <chocoapricot@nifty.com>
2025-11-30 22:29:23 +0900

    Changes to be committed: 	modified:   apps/system/settings/templatetags/help_tags.py 	modified:   templates/kintai/timesheet_detail.html

--- apps/system/settings/templatetags/help_tags.py
+++ apps/system/settings/templatetags/help_tags.py
@@ -59,9 +59,11 @@ HELP_TEXTS = {
     'client_basic_contract': '契約開始日は基本契約締結日以降である必要があります',
     
     # 勤怠関連
-    'daily_overtime_enabled': '１日にこの時間を超えると、残業時間として集計されます。',
-    'monthly_overtime_enabled': '１月の残業時間がこの時間を超えると、割増時間として集計されます。',
+    'daily_overtime_enabled': '1日にこの時間を超えると、残業時間として集計されます。',
+    'monthly_overtime_enabled': '1ヶ月の残業時間がこの時間を超えると、割増時間として集計されます。',
     'calculate_midnight_premium': '22時から5時までの勤務時間が、深夜時間として集計されます。',
+    'premium_overtime': '1ヶ月の残業時間が特定時間を超える場合',
+    'late_night_overtime': '22時から5時までの勤務時間が、深夜時間として集計されます。',
 
     # 銀行・支払い関連
     'bank_code': '4桁の数字で入力（必須）',
