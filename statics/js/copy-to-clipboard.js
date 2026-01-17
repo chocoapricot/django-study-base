ccc36ddba23db34e58baf1fd8b2a49cd4c47b42e
chocoapricot <chocoapricot@nifty.com>
2026-01-17 09:06:40 +0900

    Changes to be committed: 	modified:   CHANGELOG.md 	modified:   statics/js/copy-to-clipboard.js 	modified:   templates/client/client_detail.html 	modified:   templates/client/client_user_detail.html 	modified:   templates/common/_base.html 	modified:   templates/company/company_department_detail.html 	modified:   templates/company/company_detail.html 	modified:   templates/contract/ai_check_base.html 	modified:   templates/staff/staff_detail.html

--- statics/js/copy-to-clipboard.js
+++ statics/js/copy-to-clipboard.js
@@ -1,15 +1,21 @@
-function copyToClipboard(button) {
+function copyToClipboard(icon) {
     // ボタンの前の .copy-target を探してテキストを取得
-    const targetText = button.previousElementSibling.innerText;
+    const targetText = icon.previousElementSibling.innerText;
     navigator.clipboard.writeText(targetText).then(() => {
         // コピーが成功したらメッセージを表示
-        const copyMessage = button.nextElementSibling;
+        const copyMessage = icon.nextElementSibling;
         copyMessage.style.display = "inline";
 
-        // 2秒後にメッセージを非表示にする
+        // アイコンを変更 (bi-copy -> bi-check)
+        icon.classList.remove('bi-copy');
+        icon.classList.add('bi-check');
+
+        // 2秒後にメッセージを非表示にし、アイコンを元に戻す
         setTimeout(() => {
             copyMessage.style.display = "none";
-        }, 1000);
+            icon.classList.remove('bi-check');
+            icon.classList.add('bi-copy');
+        }, 2000);
     }).catch(err => {
         console.error("コピーに失敗しました: ", err);
     });
