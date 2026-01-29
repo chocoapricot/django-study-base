1a47ec3826133ba682b278bbd8170fbec3bc2ab6
chocoapricot <chocoapricot@nifty.com>
2026-01-29 23:09:11 +0900

    build.shに環境変数による初期セットアップ実行機能を追加

--- build.sh
+++ build.sh
@@ -8,5 +8,13 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input --settings=config.settings.product
 
-# Run migrations
+# Run migrations (Normal)
 python manage.py migrate --settings=config.settings.product
+
+# Run initial setup if INITIAL_SETUP environment variable is set to True
+if [ "$INITIAL_SETUP" = "True" ]; then
+    echo "🚀 Running initial setup script..."
+    # setup.py 内部で設定ファイルを読み込めるよう環境変数を指定
+    export DJANGO_SETTINGS_MODULE=config.settings.product
+    python _scripts/setup.py
+fi
